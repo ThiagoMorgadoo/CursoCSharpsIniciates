65a475996d3f622671b709c990dc14347a3ee9b1
Thiago Morgado <Thiago-morgado@outlook.com.br>
2018-07-02 20:42:50 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharpsIniciates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharpsIniciates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+        
+    }
+}
