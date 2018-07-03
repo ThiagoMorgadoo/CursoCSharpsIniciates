12048a1902a621dfafce3feb7b43232d5940694e
Thiago Morgado <Thiago-morgado@outlook.com.br>
2018-07-03 09:46:23 -0300

    Exercicios do if

--- CursoCSharpsIniciates/Program.cs
+++ CursoCSharpsIniciates/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,11 +12,42 @@ namespace CursoCSharpsIniciates
     {
         static void Main(string[] args)
         {
-                        
+            string nome;
+            decimal id;
+            decimal nota1;
+            decimal nota2;
+            decimal nota3;
+            decimal Faltas;
+            decimal res;
+
+            Console.WriteLine("Digite o nome seu nome ? ");
+            nome = Console.ReadLine();
+            Console.WriteLine("digite seu id");
+            id = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite A quantidade de faltas no semestre");
+            Faltas = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Informe sua nota do tea ? ");
+            nota1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a nota da sua n1 ? ");
+            nota2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a nota da sua n3 ?");
+            nota3 = Convert.ToDecimal(Console.ReadLine());
+            
+            res = (nota1 * 2 + nota2 * 2 + nota3 * 6) / 10;
+
+            if (res == 6 && Faltas == 6)
+            {
+                Console.WriteLine("Parabens vc passou de semestre..." + res);
+            }
+            else 
+            {
+                
+                Console.WriteLine("Ficou de dp.");
+            }
 
 
 
-
+            Console.ReadKey();
         }
         
     }
